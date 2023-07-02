f948c423bdac4739c045c71128a4f63d2a097210
stevieismagic <stevieismagic@gmail.com>
2023-07-02 12:58:28 -0400

    bump

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,32 +104,14 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
       <footer className="p-6 text-center text-white/80 sm:p-12">
         Thank you to{' '}
         <a
-          href="https://edelsonphotography.com/"
+          href="https://nextjs.com/"
           target="_blank"
           className="font-semibold hover:text-white"
           rel="noreferrer"
         >
-          Josh Edelson
+          Next.js
         </a>
-        ,{' '}
-        <a
-          href="https://www.newrevmedia.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Jenny Morgan
-        </a>
-        , and{' '}
-        <a
-          href="https://www.garysextonphotography.com/"
-          target="_blank"
-          className="font-semibold hover:text-white"
-          rel="noreferrer"
-        >
-          Gary Sexton
-        </a>{' '}
-        for the pictures.
+
       </footer>
     </>
   )
